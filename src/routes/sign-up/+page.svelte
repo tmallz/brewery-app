40f57c62c641043eac029b33588e676b13d27eb2
Tim O'Malley <tomalley24@gmail.com>
2022-08-21 22:40:41 -0500

    meh

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -1,5 +1,9 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Login now!</h1>
+        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      </div>
       <div class="card-body">
         <div class="form-control">
           <label class="label" for = "Email">
@@ -12,6 +16,10 @@
             <span class="label-text">Password</span>
           </label>
           <input type="text" placeholder="password" class="input input-bordered" />
+          <label class="label" for = 'Password'>
+            <span class="label-text">Confirm Password</span>
+          </label>
+          <input type="text" placeholder="confirm password" class="input input-bordered" />
           <label class="label" for = "Links">
             <ul>
               <li><a href="/login" class="label-text-alt link link-hover">Have an account already? Log In</a></li>
