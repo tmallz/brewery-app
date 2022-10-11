8c951079470b1f55b4e78288b7a1440a9502f70e
Timothy O'Malley <tomalley24@gmail.com>
2022-10-10 19:59:20 -0500

    removed boiler plate from front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,13 +43,8 @@
 
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row">
-		<img
-			src="https://placeimg.com/260/400/arch"
-			alt="pic"
-			class="max-w-sm rounded-lg shadow-2xl"
-		/>
 		<div>
-			<h1 class="text-5xl font-bold">Box Office News!</h1>
+			<h1 class="text-5xl font-bold">BrewerMe!</h1>
 			<p class="py-6">
 				Welcome to Brewery-Me. An app that allows you to find breweries near you
 				and wherever you want.
